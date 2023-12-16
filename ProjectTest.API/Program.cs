af3d9e44939234c85a6c0675641450aa823e9bf1
Johan Rojas <johanrojasn21@gmail.com>
2023-12-15 22:04:05 -0500

    Update

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "V1 Diboks enginier GET - Net 6";
+    return "V2 Diboks enginier GET - Net 6";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "V1 Diboks enginier  POST - Net 6";
+    return "V2 Diboks enginier  POST - Net 6";
 });
 
 //app.Run($"http://0.0.0.0:{port}");
